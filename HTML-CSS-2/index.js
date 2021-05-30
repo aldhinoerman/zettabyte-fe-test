2b27b748d04fe299a00bd29dbbc7d5e6376cf9f0
aldhinoerman <aldynoerman@gmail.com>
2021-05-30 20:15:51 +0700

    fixing html css

---
+++ HTML-CSS-2/index.js
@@ -0,0 +1,31 @@
+var modal = document.getElementById('myModal');
+
+var btns = document.getElementsByClassName("btn");
+
+var span = document.getElementsByClassName("close")[0];
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].onclick = function() {
+    modal.style.display = "block";
+  }
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
